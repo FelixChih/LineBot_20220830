a729e3aed8225a62f3857c741c747cd61c75411d
TGI102_02_遲名恩 <felix6258925@gmail.com>
2022-08-31 18:04:21 +0800

    update

--- app.py
+++ app.py
@@ -195,18 +195,18 @@ def getCarouselMessage(data):
           "type": "image_carousel",
           "columns": [
               {
-                "imageUrl": F"{end_point}/static/taipei_101.jpeg",
+                "imageUrl": F"{end_point}/static/test.jpeg",
                 "action": {
                   "type": "postback",
-                  "label": "台北101",
+                  "label": "法師喔",
                   "data": json.dumps(data)
                 }
               },
               {
-                "imageUrl": F"{end_point}/static/taipei_1.jpeg",
+                "imageUrl": F"{end_point}/static/test2.jpeg",
                 "action": {
                   "type": "postback",
-                  "label": "台北101",
+                  "label": "不良少年",
                   "data": json.dumps(data)
                 }
               }
