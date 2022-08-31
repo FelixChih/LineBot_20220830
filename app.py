47d977b2b17465324fc51a97c4218a1685e4f43d
TGI102_02_遲名恩 <felix6258925@gmail.com>
2022-08-31 15:21:25 +0800

    update

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ def getCallCarMessage(data):
 def getPlayStickerMessage():
     message = dict()
     message["type"] = "sticker"
-    message["packageId"] = "446""
+    message["packageId"] = "446"
     message["stickerId"] = "1988"
     return message
 
